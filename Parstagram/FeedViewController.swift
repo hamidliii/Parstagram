fd385d5bcdff102cdd8e21034011a31ebc07bf64
Nina Hamidli <nigar.hamidli1999@gmail.com>
2021-03-20 17:21:19 +0900

    A feature to display the photos on the feed.

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -54,12 +54,11 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             
             cell.captionLabel.text = post["caption"] as! String
             
-//            let imageFile = post["image"] as! PFFileObject
-//            let urlString = imageFile.url!
-//            let url = URL(string: urlString)!
-//
-//            cell.photoView.af_setImage(withURL: url)
-//
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            
+            cell.photoView.af_setImage(withURL: url)
             return cell
             }
         
